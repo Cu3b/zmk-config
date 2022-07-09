7c16f304f4eddb7833d84a1770fcc33291d20e5a
Cu3b <89937459+Cu3b@users.noreply.github.com>
2022-07-09 17:51:53 +0300

    testing keymaps

---
+++ config/layers.h
@@ -0,0 +1,13 @@
+/*
+*
+* Copyright (c) 2022 Cu3b
+* SPDX-License-Identifier: MIT
+*
+*/
+
+#define BASE    0
+#define SYM     1
+#define NUM     2
+#define FKEYS   3
+#define NAV     4
+#define SHC     5
