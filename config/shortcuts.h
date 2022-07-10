91214060c61f7b7fdcb3b6eb2f9b08da2de442bc
Cu3b <89937459+Cu3b@users.noreply.github.com>
2022-07-10 11:15:41 +0300

    testi

--- config/shortcuts.h
+++ config/shortcuts.h
@@ -5,3 +5,33 @@
 *
 */
 
+#define CtlNBsp (LC(BSPC))
+#define CtlNDel (LC(DEL))
+#define TskView (LG(TAB))
+#define TaskMgr LC(LS(ESC))       // Task manager
+
+#define PrvDstp (LG(LC(LEFT)))    // Previous virtual desktop
+#define NxtDstp (LG(LC(RGHT)))    // Next virtual desktop
+#define NewDstp (LG(LC(D)))       // New virtual desktop
+#define ClsDstp (LG(LC(F4)))      // Close virtual desktop
+#define PrevTab (LC(PG_UP))       // Previous tab
+#define NextTab (LC(PG_DN))       // Next tab
+#define RestTab (LC(LS(T)))       // Restore tab
+#define ClseTab (LC(W))           // Close tab
+
+#define SC_Lock (LG(L))           // Lock windows
+#define SC_Clse (LA(F4))          // Close window
+#define SC_Dtop (LG(D))           // Show desktop
+#define SC_Snip (LG(LS(S)))       // Windows snipping tool
+
+#define SC_Psth (LG(V))           // Paste with history
+#define SC_Undo (LC(Z))           // Undo
+#define SC_Cut  (LC(X))           // Cut
+#define SC_Copy (LC(C))           // Copy
+#define SC_Pste (LC(V))           // Paste
+#define SC_SelA (LC(A))           // Select all
+#define SC_Find (LC(F))           // Find
+#define SC_Redo (LC(LS(Z)))       // Redo
+#define SC_Save (LC(S))           // Save
+
+#define FI_BRK  LC(PAUSE_BREAK)   // Break
